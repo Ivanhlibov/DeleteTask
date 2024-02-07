07c318816d02ab108df6d3c1bab49ee22f063995
Ivanhlibov <92669197+Ivanhlibov@users.noreply.github.com>
2024-02-07 12:07:08 +0200

    end

---
+++ deletetask/src/Components/TaskList.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+class TaskList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tasks: [
+        { id: 1, text: "Task 1" },
+        { id: 2, text: "Task 2" },
+        { id: 3, text: "Task 3" }
+      ]
+    };
+  }
+
+  handleDeleteTask = (taskId) => {
+    const updatedTasks = this.state.tasks.filter(task => task.id !== taskId);
+    this.setState({ tasks: updatedTasks });
+  }
+
+  render() {
+    return (
+      <div style={{ margin: '20px' }}>
+        {this.state.tasks.map(task => (
+          <div key={task.id} style={{ marginBottom: '10px' }}>
+            {task.text}
+            <button onClick={() => this.handleDeleteTask(task.id)}>Видалити</button>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+function App() {
+  return (
+    <div>
+      <h1>Список завдань</h1>
+      <TaskList />
+    </div>
+  );
+}
+
+export default App;
